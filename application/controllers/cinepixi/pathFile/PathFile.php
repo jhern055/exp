dc2726b1e2953a347905be82789ba005f57aebe6
Daniel <jhern@gmail.com>
2016-08-31 23:54:17 -0500

    change

--- application/controllers/cinepixi/pathFile/PathFile.php
+++ application/controllers/cinepixi/pathFile/PathFile.php
@@ -271,10 +271,8 @@ foreach($_SESSION["paths"] as $path){
 	$this->load->model("cinepixi/pathFile/pathFile_model");
 	$path_folder=$this->pathFile_model->get_cinepixi_pathFile_to_option(null,"");
 
-	if(count($path_folder)!=$_SESSION["foreach_path"])
-	$_SESSION["foreach_path"]="";
 
-	if(!empty($path_folder) and count($path_folder)!=$_SESSION["foreach_path"])
+	if(!empty($path_folder))
 	foreach ($path_folder as $key => $path) {
 				// insertarmos en movie o aztualizamos
 
